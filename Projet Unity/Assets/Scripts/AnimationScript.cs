76c36362b7e1e11db0552aadf2453a12588a2dab
LilCartell <lilian.cartellier01@orange.fr>
2015-03-05 19:40:24 +0100

    Prefab bouquin animé

---
+++ Projet Unity/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+
+	private Animator animator;
+
+	void Awake()
+	{	
+		// Get the animator
+		animator = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter(){
+		animator.SetBool("MouseOver", true);
+	}
+
+	void OnMouseExit(){
+			animator.SetBool("MouseOver", false);
+	}
+}
